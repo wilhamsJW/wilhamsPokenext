dadf7c851e1c783f9c53f4bc9380f2134847a41f
wilhams junior <wilhams.junior@mundiale.com.br>
2023-04-28 18:47:29 -0300

    project structuring

---
+++ my-app/src/pages/test.js
@@ -0,0 +1,8 @@
+export default function NavBar() {
+    return (
+        <h1>Teste para acesssar uma página, todos os arquivos criado dentro de pages, atoumaticamente serão uma rota, q para acessar devemos usar
+            o nome deste arquivo como a rota, esse arqivo se chama test.js e para acessálo basta colocar isso: href={`/test`}
+            {/**<Link href={`/test`}>Teste rota</Link> no seu JSX*/}
+        </h1>
+    )
+}
