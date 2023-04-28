bcd0c2053bb04a645034bdb9c3dc8cf315759647
wilhams junior <wilhams.junior@mundiale.com.br>
2023-04-28 18:28:59 -0300

    deployment correction

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,9 +1,8 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
 import Layout from '../components/Layout'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} /> {/** renderiza o que está dentro do index.tsx */}
