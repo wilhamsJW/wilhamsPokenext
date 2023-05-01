93b88a726f26e4f110bd9075975bc2415299b4aa
wilhams junior <wilhams.junior@mundiale.com.br>
2023-05-01 18:15:44 -0300

    feat: fetch data react query

--- my-app/src/pages/pokemon/[pokemonID].js
+++ my-app/src/pages/pokemon/[pokemonID].js
@@ -8,75 +8,28 @@ import { useRouter } from "next/router";
 
 import { useQuery } from '@tanstack/react-query'
 
-// Faz mapeamento geral dos dados
-// estou passando todos os meus paths para função para q possa pré renderizar
-export const getStaticPaths = async () => {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=180`);
-  const data = await res.json();
 
-  // params
-  const paths = data.results.map((e, i) => {
-    return {
-      params: { pokemonID: (i + 1).toString() },
-      /** Definindo o ID que irei passar como path, coloquei i+1 pq o array
-       * começa em 0, então o pokemon zero do array será o pokemon 1 para mim
-       * dessa forma percorro todos os id da minha rota e mando eles no return abaixo
-       */
-    };
-  });
-
-  return {
-    paths,
-    //fallback: false /** é padrão do next.js porém pode ser usado como true, veja documentação */,
-    fallback: true
-    /** Agora estou usando como true pq quero pegar dados da API não renderizado
-     * para que o site não fique tão lento adicionei um limit no endpoint de 180 elementos e esses
-     * serão os dados pré renderizados e não demora a carregar para o usuário ver, porém existem mais páginas no site,
-     * para fazer com que eles apareçam apenas quando solicitadas eu configuro fallback para true e desta forma consigo pegar qq elemento vindo da API
-     *
-     * Com fallback: false só é permitido pegar 180 elementos, se eu tentar colocar na url 181 irá retornar 404 página de erro
-     */,
-  };
-};
-
-/**************************************************************************
- * ************* Removed getStaticProps to do with React Query ************
- **************************************************************************/
-
-// faz mapeamento um a um dos dados
-// export const getStaticProps = async (context) => {
-//   // esse context é basicamente o que criamos acima, por isso acessamos: context.params.pokemonID
-//   //que é o mesmo nome q coloamos acima
-//   const id = context.params.pokemonID;
-//   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-
-//   const data = await res.json();
-
-//   return {
-//     props: { data },
-//   };
-// };
-
-/**************************************************************************
- * ************************ With React Query ******************************
- **************************************************************************/
-
-
-
-//export default function Pokemon({ data }) { Used with getStaticProps()
   export default function Pokemon() { // with React Query
 
-  // Next automaticamente entende que esse parâmetro { data } é da função getStaticProps()
+    const idPokemon = useRouter().query.pokemonID
+    const { isLoading, error, data } = useQuery({
+      queryKey: ['qqcois'],
+      queryFn: () =>
+        fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`).then(
+          (res) => res.json(),
+        ),
+    })
+
+    if (error) return 'An error has occurred: ' + error.message
+
   return (
     <>
-    {/** Adicionamos esse if em conjunto com fallback true para buscar dados nao renderizados
-     * We add this if in conjunction with fallaback true to fetch unrendered data */}
-    {useRouter().isFallback && (<div className={styles.loading}><div className={styles.loadingAux}></div></div>)}
+    {isLoading && (<div className={styles.loading}><div className={styles.loadingAux}></div></div>)}
 
-    {!useRouter().isFallback && <div className={styles.pokemon_container}>
+    {!isLoading && <div className={styles.pokemon_container}>
       <h1 className={styles.title}>{data.name}</h1>
       <Image
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`}
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${idPokemon}.png`}
         width="200"
         height="200"
         alt={data.name}
