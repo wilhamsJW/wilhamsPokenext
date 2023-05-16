c21fe76ea793ebf9918d4851a0fa7be24a25514b
wilhams junior <wilhams.junior@mundiale.com.br>
2023-05-15 23:05:00 -0300

    feat: added hydrate and ReactQueryDevtools

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -3,17 +3,22 @@ import type { AppProps } from 'next/app'
 
 import Layout from '../../components/Layout'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { Hydrate } from 'react-query';
+import { ReactQueryDevtools } from "react-query/devtools";
+
 
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}> {/** default React query */}
-      <Layout>
-        <Component {...pageProps} /> {/** renderiza o que está dentro do index.tsx */}
-      </Layout>
-    </QueryClientProvider>
-
+      <QueryClientProvider client={queryClient}> {/** default React query */}
+        <Hydrate state={pageProps.dehydratedState}>
+          <Layout>
+            <Component {...pageProps} /> {/** renderiza o que está dentro do index.tsx */}
+          </Layout>
+        </Hydrate>
+        <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
   )
 }
