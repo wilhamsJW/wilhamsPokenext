269ee81c0138ddb60e5802d7bd9b9b4e9de87827
wilhams junior <wilhams.junior@mundiale.com.br>
2023-06-03 00:25:12 -0300

    style: organization

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -8,28 +8,19 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import { Hydrate } from 'react-query'
 import { ReactQueryDevtools } from "react-query/devtools"
 
-
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       cacheTime: Infinity, // Mantém os dados em cache indefinidamente
-//     },
-//   },
-// })
-
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <QueryClientProvider client={queryClient}> {/** default React query */}
-        <Hydrate state={pageProps.dehydratedState}>
+    <QueryClientProvider client={queryClient}> {/** default React query */}
+      <Hydrate state={pageProps.dehydratedState}>
           <Layout>
             <Component {...pageProps} /> {/** renderiza o que está dentro do index.tsx ou index.js | estou passando <Component {...pageProps} />
              * como prop para que o layout receba ele no componente dele e renderize onde queira
              */}
           </Layout>
-        </Hydrate>
-        <ReactQueryDevtools initialIsOpen />
-      </QueryClientProvider>
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
   )
 }
