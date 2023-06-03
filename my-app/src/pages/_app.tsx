d24d96b877272b49605761d0d4bf9f718185648b
wilhams junior <wilhams.junior@mundiale.com.br>
2023-06-02 22:38:37 -0300

    style: remove request react query

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -1,12 +1,21 @@
 import '@/styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
 import Layout from '../../components/Layout'
 
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { Hydrate } from 'react-query';
-import { ReactQueryDevtools } from "react-query/devtools";
+import { Hydrate } from 'react-query'
+import { ReactQueryDevtools } from "react-query/devtools"
 
+
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       cacheTime: Infinity, // Mantém os dados em cache indefinidamente
+//     },
+//   },
+// })
 
 const queryClient = new QueryClient()
 
@@ -15,7 +24,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <QueryClientProvider client={queryClient}> {/** default React query */}
         <Hydrate state={pageProps.dehydratedState}>
           <Layout>
-            <Component {...pageProps} /> {/** renderiza o que está dentro do index.tsx */}
+            <Component {...pageProps} /> {/** renderiza o que está dentro do index.tsx ou index.js | estou passando <Component {...pageProps} />
+             * como prop para que o layout receba ele no componente dele e renderize onde queira
+             */}
           </Layout>
         </Hydrate>
         <ReactQueryDevtools initialIsOpen />
